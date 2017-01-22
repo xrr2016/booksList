7cf8e42682bf6fa845737ff5eca14a4c6600523d
xrr2016 <13286904669@163.com>
2017-01-22 21:54:09 +0800

    :zap: edit

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,48 @@
 <template>
-<div id="app" class="container-fluid">
-  <h1 class="title">
+  <div id="app" class="container-fluid">
+    <h1 class="title">
         <img class="logo" src="./assets/logo.png" alt="">
-        {{ title }}
-        <img src="https://docs.wilddog.com/images/logo-d2df5d3b45.svg" height="40" alt="">
-      </h1>
-  <table class="table">
-    <thead>
-      <tr>
-        <th>书名</th>
-        <th>作者</th>
-        <th>简介</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="book of books">
-        <td>{{ book.name }}</td>
-        <td>{{ book.author }}</td>
-        <td>{{ book.subject }}</td>
-      </tr>
-    </tbody>
-  </table>
-</div>
-</div>
-</div>
+          {{ title }}
+        <img src="https://docs.wilddog.com/images/logo-d2df5d3b45.svg" height="40" alt=""></h1>
+
+    <div class="row">
+      <table class="table">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>书名</th>
+            <th>作者</th>
+            <th>简介</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(book,index) of books">
+            <td>{{ index }}</td>
+            <td><a :href="book.url">{{ book.name }}</a></td>
+            <td>{{ book.author }}</td>
+            <td class="col-md-4 subject">{{ book.subject }}</td>
+            <td><button type="button" class="btn btn-danger">删除</button></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+
+    <form >
+      <div class="form-group">
+        <label for="">书名:{{newBook.name}}</label>
+        <input type="text" class="form-control"  placeholder="请输入书名" v-model="newBook.name">
+      </div>
+      <div class="form-group">
+        <label for="">作者:{{newBook.author}}</label>
+        <input type="text" class="form-control"  placeholder="请输入作者名" v-model="newBook.author">
+      </div>
+      <div class="form-group">
+        <label for="">简介:</label>
+        <input type="text" class="form-control"  placeholder="请输入简介" v-model="newBook.subject">
+      </div>
+      <button type="button" name="button" class="btn btn-success btn-fluid">添加书籍</button>
+    </form>
+  </div>
 </template>
 
 <script>
@@ -41,9 +60,12 @@ export default {
   name: 'app',
   data() {
     return {
-      title: 'Vue.js Wilddog BookList',
+      title: 'BookList',
       newBook: {
-
+        name:'',
+        author:'',
+        subject:'',
+        url:''
       }
     }
   },
@@ -61,15 +83,18 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  padding-top: 60px;
+  padding-top: 12px;
   color: #2c3e50;
 }
 
 .logo {
   height: 40px;
 }
-
 .title {
+  padding:12px;
   text-align: center;
+}
+.subject{
+
 }
 </style>
